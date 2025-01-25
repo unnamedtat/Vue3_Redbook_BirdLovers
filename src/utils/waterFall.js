fecf64f34cf068d20b2942e41174f7e8942eaf60
unnamedtat <2641167724@qq.com>
2025-01-26 00:05:25 +0800

    refactor(waterfall): 优化瀑布流布局初始化，适配Vue

--- src/utils/waterFall.js
+++ src/utils/waterFall.js
@@ -1,65 +1,61 @@
-export function waterFallInit(columns, card_columns, arrHeight, cards) {
-    columns.value = waterfall_column()
-    card_columns.value = {}
-    arrHeight.value = []
-    for (let i = 0; i < columns.value; i++) {
-        card_columns.value[i] = []
-    }
-    for (let i = 0; i < cards.value.length; i++) {
-        var height = cards.value[i].img_info.height / (cards.value[i].img_info.width / 250)
-        if (i < columns.value) {
-            card_columns.value[i].push(cards.value[i])
-            arrHeight.value.push(height)
-        } else {
-            let obj = {
-                minH: arrHeight.value[0],
-                minI: 0
-            }
-            for (let j = 0; j < arrHeight.value.length; j++) {
-                if (arrHeight.value[j] < obj.minH) {
-                    obj.minH = arrHeight.value[j]
-                    obj.minI = j
-                }
-            }
-            card_columns.value[obj.minI].push(cards.value[i])
-            arrHeight.value[obj.minI] += height
-        }
-    }
-}
+import { ref } from 'vue'
 
-export function waterFallMore(arrHeight, card_columns, more) {
+export class WaterFall {
+  constructor(columns, card_columns, cards) {
+    this.columns = columns
+    this.card_columns = card_columns
+    this.arrHeight = ref([])
+    this.cards = cards
+  }
+  // 初始化瀑布流布局
+  init() {
+    this.card_columns.value = {}
+    this.arrHeight.value = []
+    // 初始化列
+    this.card_columns.value = Array.from({ length: this.columns.value }, () => [])
+    // 分配卡片到最短的列
+    for (let i = 0; i < this.cards.value.length; i++) {
+      const height = this._calculateCardHeight(this.cards.value[i])
+
+      if (i < this.columns.value) {
+        this.card_columns.value[i].push(this.cards.value[i])
+        this.arrHeight.value.push(height)
+      } else {
+        const { minI } = this._findShortestColumn()
+        this.card_columns.value[minI].push(this.cards.value[i])
+        this.arrHeight.value[minI] += height
+      }
+    }
+  }
+
+  // 加载更多卡片
+  loadMore(more) {
     for (let i = 0; i < more.length; i++) {
-        var height = more[i].img_info.height / (more[i].img_info.width / 250)
-        let obj = {
-            minH: arrHeight.value[0],
-            minI: 0
-        }
-        for (let j = 0; j < arrHeight.value.length; j++) {
-            if (arrHeight.value[j] < obj.minH) {
-                obj.minH = arrHeight.value[j]
-                obj.minI = j
-            }
-        }
-        card_columns.value[obj.minI].push(more[i])
-        arrHeight.value[obj.minI] += height
-    }
-}
+      const height = this._calculateCardHeight(more[i])
+      const { minI } = this._findShortestColumn()
 
-export function resizeWaterFall(columns, card_columns, arrHeight, cards) {
-    var timerId = null;
-    window.onresize = function () {
-        if (timerId) {
-            clearTimeout(timerId);
-        }
-        timerId = setTimeout(() => {
-            waterFallInit(columns, card_columns, arrHeight, cards)
-        }, 300)
+      this.card_columns.value[minI].push(more[i])
+      this.arrHeight.value[minI] += height
     }
-}
+  }
 
-function waterfall_column() {
-    // 1.获取主容器的宽度
-    var content = document.getElementById('content')
-    var contentWidth = content.offsetWidth
-    return Math.floor(contentWidth / (280))
+  calculate(contentWidth) {
+    const width = Math.floor(contentWidth / 280)
+    this.columns.value = width > 1 ? width : 1 // 最少一列
+  }
+
+  // 计算卡片高度
+  _calculateCardHeight(card) {
+    return card.img_info.height / (card.img_info.width / 250)
+  }
+
+  // 找到最短的列
+  _findShortestColumn() {
+    const { minH, minI } = this.arrHeight.value.reduce(
+        (acc, val, index) =>
+            (val < acc.minH ? { minH: val, minI: index } : acc),
+        { minH: Infinity, minI: 0 }
+    );
+    return {minH,minI}
+  }
 }
