16224190ea7d3d4db1f7269a9d5963fef1ba0514
unnamedtat <2641167724@qq.com>
2025-01-26 11:19:44 +0800

    feat(backPage): 封装返回按钮

---
+++ src/components/backPage.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Back } from '@element-plus/icons-vue'
+</script>
+<template>
+  <el-button class="backPage" type="info" :icon="Back"></el-button>
+</template>
+
+<style scoped lang="scss">
+.backPage {
+  position: fixed;
+  top: 5%;
+  left: 3%;
+  justify-content: center;
+  align-items: center;
+  width: 2.9rem;
+  height: 2.9rem;
+  border-radius: 50%;
+  transition: all 0.3s;
+  z-index: 4;
+}
+</style>
