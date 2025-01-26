679eaddb102d7d4b88753fac0c669349b76ab1f2
unnamedtat <2641167724@qq.com>
2025-01-26 11:02:46 +0800

    feat(ui): 统一 Element Plus 配置并增加夜间模式

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,42 @@
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // element-plus
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
-
-
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        // element-plus
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            // 1.配置elementPlus采用sass
-            resolvers: [ElementPlusResolver({importStyle: "sass"})],
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+  plugins: [
+    vue(),
+    // element-plus
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      // 1.配置elementPlus采用sass
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' })]
+    })
+  ],
+  server: {
+    port: 5173
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+          @use "@/styles/var.scss" as *;
+        `
+      }
+    }
+  }
 })
