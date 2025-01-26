6079e267541f5e6299c7d632983ab938088ccdee
unnamedtat <2641167724@qq.com>
2025-01-26 11:06:04 +0800

    feat(cardSkeleton): 卡片骨架屏组件

---
+++ src/components/cardSkeleton.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  loading: {
+    type: Boolean,
+    required: true
+  },
+  columns: {
+    type: Number,
+    required: true
+  },
+  rows: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <el-skeleton :loading="loading">
+    <template #template>
+      <section v-for="i in columns">
+        <div v-for="j in rows" class="skeleton-div">
+          <el-skeleton-item
+            variant="rect"
+            class="skeleton-img"
+            :style="{
+              height:
+                i % 2 === 1
+                  ? j === 1
+                    ? '280px'
+                    : j === 2
+                      ? '240px'
+                      : '200px'
+                  : j === 1
+                    ? '220px'
+                    : j === 2
+                      ? '260px'
+                      : '190px'
+            }"
+          />
+          <div class="skeleton-text">
+            <el-skeleton-item variant="p" style="width: 70%" />
+            <el-skeleton-item variant="p" style="width: 40%" />
+          </div>
+        </div>
+      </section>
+    </template>
+    <slot></slot>
+  </el-skeleton>
+</template>
+
+<style scoped lang="scss">
+.el-skeleton {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  .skeleton-div {
+    .skeleton-img {
+      border-radius: $card-border-radius;
+      width: $card-image-width;
+    }
+    .skeleton-text div {
+      padding: 0.5rem;
+    }
+  }
+}
+</style>
