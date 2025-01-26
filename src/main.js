2b89be79a55aa86b5c194a7bd9c707b4bb36a687
unnamedtat <2641167724@qq.com>
2025-01-26 11:22:51 +0800

    feat(plugin): 全局注册插件、导入样式文件

--- src/main.js
+++ src/main.js
@@ -1,22 +1,17 @@
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 import '@/assets/font/iconfont.css'
-import '@/styles/common.css'
-import asideNav from '@/components/asideNav.vue'
-import headNav from '@/components/headNav.vue'
-import homeCard from '@/components/homeCard.vue'
-import cardDetail from '@/components/cardDetail.vue'
+import '@/styles/common.scss'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import '@/styles/element/dark.scss'
+import { componentPlugin } from '@/components'
+import { lazyPlugin } from '@/directives'
 
 const app = createApp(App)
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
-app.use(pinia).use(router).mount('#app')
-app.component('asideNav', asideNav)
-    .component('headNav', headNav)
-    .component('homeCard', homeCard)
-    .component('cardDetail', cardDetail)
+app.use(pinia).use(router).use(componentPlugin).use(lazyPlugin).mount('#app')
