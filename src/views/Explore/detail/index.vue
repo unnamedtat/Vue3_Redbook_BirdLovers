c55d6625c16dbc1d4731b2283f2458d7539083cd
unnamedtat <2641167724@qq.com>
2025-01-26 11:46:03 +0800

    refactor(explore): 精简冗余代码

---
+++ src/views/Explore/detail/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+  detail: {
+    type: Object,
+    required: true
+  },
+  afterDoComment: {
+    type: Function,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <BLCardDetail :detail="detail" @afterDoComment="afterDoComment" ref="overlay" />
+</template>
+
+<style scoped lang="scss"></style>
