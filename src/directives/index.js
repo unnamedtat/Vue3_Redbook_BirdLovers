c06f6b7dd6a759598b06b47b609aa12f0537d2fa
unnamedtat <2641167724@qq.com>
2025-01-26 11:06:51 +0800

    feat(plugin): 添加图片懒加载插件

---
+++ src/directives/index.js
@@ -0,0 +1,18 @@
+// 定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const lazyPlugin = {
+  install(app) {
+    app.directive('img-lazy', {
+      mounted(el, binding) {
+        const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
+          if (isIntersecting) {
+            // 进入视口区域
+            el.src = binding.value
+            stop()
+          }
+        })
+      }
+    })
+  }
+}
