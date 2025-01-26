6ba8f6fb8e1f747b12feaafb6e170d3870d9ab8b
unnamedtat <2641167724@qq.com>
2025-01-26 11:46:54 +0800

    style(project): 统一代码格式

--- src/router/index.js
+++ src/router/index.js
@@ -1,98 +1,99 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
 
-const Home = () => import('@/views/Home/index.vue');
-const Login = () => import('@/views/Login/index.vue');
-const Detail = () => import('@/views/Details/index.vue');
-const Explore = () => import('@/views/Explore/index.vue');
-const UserIndex = () => import('@/views/UserIndex/index.vue');
-const Uploads = () => import('@/views/Uploads/index.vue');
-const NotFound = () => import('@/views/NotFound/index.vue');
-const UserPostControl = () => import('@/views/UserPostControl/index.vue');
-import {useUserStore} from "@/stores/user";
+const Home = () => import('@/views/Home/index.vue')
+const Login = () => import('@/views/Login/index.vue')
+const Detail = () => import('@/views/Explore/detail/index.vue')
+const Explore = () => import('@/views/Explore/index.vue')
+const UserIndex = () => import('@/views/UserIndex/index.vue')
+const Uploads = () => import('@/views/Uploads/index.vue')
+const NotFound = () => import('@/views/NotFound/index.vue')
+const UserPostControl = () => import('@/views/UserPostControl/index.vue')
+import { useUserStore } from '@/stores/useUserStore'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: Home,
+      meta: {
+        title: '欢迎您，今天观鸟没？'
+      },
+      children: [
         {
-            path: '/',
-            component: Home,
-            meta: {
-                title: '欢迎来到Dlock~',
-            },
-            children: [
-                {
-                    path: '',
-                    component: Explore,
-                    meta: {
-                        title: '欢迎来到Dlock~',
-                    },
-                },
-                {
-                    path: 'explore/:id',
-                    component: Detail,
-                    meta: {
-                        title: '这里是卡片详情页！',
-                    },
-                },
-                {
-                    path: 'user/index/:id',
-                    component: UserIndex,
-                    meta: {
-                        title: 'Dlock-分享你的生活',
-                    },
-                },
-                {
-                    path: 'user/uploads',
-                    component: Uploads,
-                    meta: {
-                        title: '发布 .Dlock',
-                    },
-                },
-                {
-                    path: 'user/control',
-                    component: UserPostControl,
-                    meta: {
-                        title: '信息管理 .Dlock',
-                    },
-                },
-            ],
+          path: '',
+          component: Explore,
+          meta: {
+            title: '欢迎您，今天观鸟没？'
+          },
+          children: [
+            {
+              path: 'explore/:id',
+              component: Detail,
+              meta: {
+                title: '这里是卡片详情页！'
+              }
+            }
+          ]
         },
         {
-            path: '/login',
-            component: Login,
-            meta: {
-                title: '欢迎登录Dlock分享你的生活',
-            },
+          path: 'user/index/:id',
+          component: UserIndex,
+          meta: {
+            title: 'Bird Lovers-分享可爱的小鸟'
+          }
         },
         {
-            path: '/:catchAll(.*)',
-            component: NotFound,
-            meta: {
-                title: '你想找什么呢？',
-            },
+          path: 'user/uploads',
+          component: Uploads,
+          meta: {
+            title: '发布 .Bird Lovers'
+          }
         },
-    ],
-});
-router.beforeEach((to, from, next) => {
-    // 获取目标路由的相关信息，例如路由元信息 meta
-    const {meta} = to;
-
-    // 获取用户信息
-    const userStore = useUserStore();
-    const userInfo = userStore.userInfo;
-
-    // 根据用户信息动态设置网页标题
-    if (userInfo.username && meta.title) {
-        meta.title = `${userInfo.username} - ${meta.title}`;
+        {
+          path: 'user/control',
+          component: UserPostControl,
+          meta: {
+            title: '信息管理 .Bird Lovers'
+          }
+        }
+      ]
+    },
+    {
+      path: '/login',
+      component: Login,
+      meta: {
+        title: '欢迎成为Bird Lovers分享你的生活'
+      }
+    },
+    {
+      path: '/:catchAll(.*)',
+      component: NotFound,
+      meta: {
+        title: '你想找什么呢？'
+      }
     }
-    // 继续导航
-    next();
-});
+  ]
+})
+router.beforeEach((to, from, next) => {
+  // 获取目标路由的相关信息，例如路由元信息 meta
+  const { meta } = to
 
+  // 获取用户信息
+  const userStore = useUserStore()
+  const userInfo = userStore.userInfo
+
+  // 根据用户信息动态设置网页标题
+  if (userInfo.username && meta.title) {
+    meta.title = `${userInfo.username} - ${meta.title}`
+  }
+  // 继续导航
+  next()
+})
 
 router.afterEach((to, from) => {
-    // 根据当前路由信息来设置新的 title
-    document.title = to.meta.title || '欢迎来到Dlock~';
-});
+  // 根据当前路由信息来设置新的 title
+  document.title = to.meta.title || '欢迎Bird Lover~'
+})
 
 export default router
--- src/views/NotFound/index.vue
+++ src/views/NotFound/index.vue
@@ -1,16 +1,14 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
   <div class="not-found">
-    <h1 style="color:red;">404 - 页面不存在</h1>
+    <h1 style="color: red">404 - 页面不存在</h1>
     <p>你输入了一个错误的网址哦.</p>
     <router-link to="/">返回主页</router-link>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .not-found {
   text-align: center;
   padding: 20px;
